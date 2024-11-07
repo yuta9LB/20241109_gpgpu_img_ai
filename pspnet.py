def50f23daa967ee5bff65363f3e640f2588b3f7
yuta9LB <y.tkym061596@gmail.com>
2024-11-07 14:09:59 +0900

    add train files for pspnet

--- pspnet.py
+++ pspnet.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 
 class ConvBatchNorm(nn.Module):
     def __init__(self, in_channels, out_channels, kernel_size, stride, padding, dilation, bias):
-        super(ConvBatchNormRelu, self).__init__()
+        super(ConvBatchNorm, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size, stride, padding, dilation, bias=bias)
         self.batchnorm = nn.BatchNorm2d(out_channels)
     
@@ -129,7 +129,7 @@ class ResidualBlockPSP(nn.Sequential):
 
         # bottleNeckIdentifyPSPの繰り返しの用意
         for i in range(n_blocks-1):
-            self.add_module("block"+str(i+2), bottleNeckIdentifyPSP(out_channels, mid_channels, out_channels, stride, dilation))
+            self.add_module("block"+str(i+2), bottleNeckIdentifyPSP(out_channels, mid_channels, stride, dilation))
 
 class DecodePSPFeature(nn.Module):
     def __init__(self, height, width, n_classes):
@@ -181,17 +181,17 @@ class PSPNet(nn.Module):
 
         # 4つのモジュールを構成するサブネットワーク
         self.feature_conv = FeatureMap_Conv()
-        self.feature_res1 = ResidualBlockPSP(n_block=block_config[0], in_channels=128, mid_channels=64, out_channels=256, stride=1, dilation=1)
-        self.feature_res2 = ResidualBlockPSP(n_block=block_config[1], in_channels=256, mid_channels=128, out_channels=512, stride=2, dilation=1)
-        self.feature_dilated_res1 = ResidualBlockPSP(n_block=block_config[2], in_channels=512, mid_channels=256, out_channels=1024, stride=1, dilation=2)
-        self.feature_dilated_res2 = ResidualBlockPSP(n_block=block_config[3], in_channels=1024, mid_channels=512, out_channels=2048, stride=1, dilation=4)
+        self.feature_res1 = ResidualBlockPSP(n_blocks=block_config[0], in_channels=128, mid_channels=64, out_channels=256, stride=1, dilation=1)
+        self.feature_res2 = ResidualBlockPSP(n_blocks=block_config[1], in_channels=256, mid_channels=128, out_channels=512, stride=2, dilation=1)
+        self.feature_dilated_res1 = ResidualBlockPSP(n_blocks=block_config[2], in_channels=512, mid_channels=256, out_channels=1024, stride=1, dilation=2)
+        self.feature_dilated_res2 = ResidualBlockPSP(n_blocks=block_config[3], in_channels=1024, mid_channels=512, out_channels=2048, stride=1, dilation=4)
 
         self.pyramid_pooling = PyramidPooling(in_channels=2048, pool_sizes=[6, 3, 2, 1], height=input_size//8, width=input_size//8)
         self.decode_feature = DecodePSPFeature(height=input_size, width=input_size, n_classes=n_classes)
 
         self.aux = AuxiliaryPSPlayers(in_channels=1024, height=input_size, width=input_size, n_classes=n_classes)
 
-    def forward(self):
+    def forward(self, x):
         x = self.feature_conv(x)
         x = self.feature_res1(x)
         x = self.feature_res2(x)
@@ -204,4 +204,4 @@ class PSPNet(nn.Module):
         x = self.pyramid_pooling(x)
         output = self.decode_feature(x)
 
-        return (output, output_aux)
+        return output, output_aux
---
+++ train_psp.py
@@ -0,0 +1,146 @@
+import os
+import math
+import tqdm
+import torch
+import numpy as np
+from PIL import Image
+from torch.utils.data import DataLoader
+
+from pspnet import PSPNet
+from dataset import VOCDataset
+from util import DiceCrossEntropyLoss
+
+# VOC2012で用いるラベル
+CLASSES = ['backgrounds','aeroplane','bicycle','bird','boat','bottle',
+            'bus','car' ,'cat','chair','cow', 
+            'diningtable','dog','horse','motorbike','person', 
+            'potted plant', 'sheep', 'sofa', 'train', 'monitor','unlabeld'
+            ]
+
+# カラーパレットの作成
+COLOR_PALETTE = np.array(Image.open("./VOCdevkit/VOC2012/SegmentationClass/2007_000170.png").getpalette()).reshape(-1,3)
+COLOR_PALETTE = COLOR_PALETTE.tolist()[:len(CLASSES)]
+
+def train(dataloader, model, optimizer, criterion, aux_weight, device):
+    model.train()
+    size = len(dataloader.dataset)
+    batch_size = dataloader.batch_size
+    step_total = math.ceil(size/batch_size)
+    total_loss = 0.0
+
+    with tqdm.tqdm(enumerate(dataloader), total=step_total) as pbar:
+        for batch, item in pbar:
+            inp, gt = item[0].to(device), item[1].to(device)
+
+            # 損失誤差を計算
+            pred, pred_aux = model(inp)
+
+            loss = criterion(pred, gt) + aux_weight * criterion(pred_aux, gt)
+            total_loss += loss.item()
+
+            # バックプロパゲーション
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            # プログレスバーに損失を表示
+            pbar.set_postfix(loss=loss.item())
+
+        avg_loss = total_loss / step_total
+        print(f"Train Loss: {avg_loss:.4f}")
+    
+    return avg_loss
+
+def test(dataloader, model, criterion, aux_weight, device):
+    model.eval()
+    size = len(dataloader.dataset)
+    batch_size = dataloader.batch_size
+    step_total = math.ceil(size/batch_size)
+    total_loss = 0.0
+
+    with torch.no_grad():
+        for item in dataloader:
+            inp, gt = item[0].to(device), item[1].to(device)
+            pred, pred_aux = model(inp)
+            loss = criterion(pred, gt) + aux_weight * criterion(pred_aux, gt)
+            total_loss += loss.item()
+
+        avg_loss = total_loss / step_total
+        print(f"Val Loss: {avg_loss:.4f}")
+
+    return avg_loss
+
+def main():
+    save_dir = './savedir'
+    save_name_prefix = 'savename'
+    csv_path = os.path.join(save_dir, f'{save_name_prefix}.csv')
+
+    initial_epoch = 0
+    chkp_path = None #'weights/psp_241107_00.pth'
+    continuation = False # 続きから再開する場合True
+
+    train_list_path = 'VOCdevkit/VOC2012_sample/listfile/train_list_1464.txt'
+    val_list_path = 'VOCdevkit/VOC2012_sample/listfile/val_list_1449.txt'
+
+    img_dir = 'VOCdevkit/VOC2012_sample/JPEGImages'
+    gt_dir = 'VOCdevkit/VOC2012_sample/SegmentationClass'
+
+    with open(train_list_path, 'r') as f:
+        train_list = f.read().splitlines()
+    with open(val_list_path, 'r') as g:
+        val_list = g.read().splitlines()
+
+    train_ds = VOCDataset(img_list=train_list, img_dir=img_dir, gt_dir=gt_dir, data_augmentation=True)
+    val_ds = VOCDataset(img_list=val_list, img_dir=img_dir, gt_dir=gt_dir)
+    print(f"len(train_data): {train_ds.__len__()}")
+    print(f"len(test_data): {val_ds.__len__()}")
+
+    train_dl = DataLoader(train_ds, batch_size=16, num_workers=8, shuffle=True)
+    val_dl = DataLoader(val_ds, batch_size=16, num_workers=8, shuffle=True)
+
+    epochs = 100
+    device = 'cuda' if torch.cuda.is_available()  else "cpu"
+    print("Using {} device".format(device))
+
+    model = PSPNet(n_classes=22)
+    model = model.to(device)
+
+    # 重みの読み込み。
+    if chkp_path is not None:
+        checkpoint = torch.load(chkp_path, map_location='cpu', weights_only=True)
+        model.load_state_dict(checkpoint['model_state_dict'], strict=False)
+        if continuation == True:
+            initial_epoch = checkpoint["epoch"] + 1
+    
+    aux_weight = 0.4
+    weight = None # torch.tensor([0.3, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 0.5])
+    if weight is not None:
+        weight = weight.to(device)
+    criterion =  DiceCrossEntropyLoss(weight=weight, dice_weight=0.5)
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.01)
+
+    if not os.path.exists(save_dir):
+        os.mkdir(save_dir)
+
+    for epoch in range(initial_epoch, epochs):
+        print(f"Epoch {epoch}\n-------------------------------")
+        train_loss = train(train_dl, model, optimizer, criterion, aux_weight, device)
+        val_loss = test(val_dl, model, criterion, aux_weight, device)
+
+        torch.save(
+        {
+            "epoch": epoch,
+            "model_state_dict": model.state_dict(),
+            "optimizer_state_dict": optimizer.state_dict(),
+            },
+        os.path.join(save_dir, f'{save_name_prefix}_ep{epoch}.pth'),
+        )
+        
+        if not os.path.exists(csv_path):
+            with open(csv_path, 'w') as f:
+                f.write('epoch,train_loss,val_loss\n')
+        with open(csv_path, 'a') as f:
+            f.write(f'{epoch},{train_loss},{val_loss}\n')
+
+if __name__ == '__main__':
+    main()
