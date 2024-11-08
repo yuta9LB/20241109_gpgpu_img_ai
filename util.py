37bfc214c7f2d1b4dc3a00e30d936e731b25613d
yuta9LB <y.tkym061596@gmail.com>
2024-11-08 18:01:01 +0900

    add iou

--- util.py
+++ util.py
@@ -14,7 +14,37 @@ CLASSES = ['backgrounds','aeroplane','bicycle','bird','boat','bottle',
 # カラーパレットの作成
 COLOR_PALETTE = np.array(Image.open("./VOCdevkit/VOC2012_sample/SegmentationClass/2007_000170.png").getpalette()).reshape(-1,3)
 COLOR_PALETTE = COLOR_PALETTE.tolist()[:len(CLASSES)]
+
+def compute_iou(pred, target, ignore_index=0):
+    # クラスごとのスコアを持つpredを最大スコアのインデックスに変換
+    pred = torch.argmax(pred, dim=1)  # サイズ：[1, 256, 256]
+
+    ious = []
+    for cls in range(len(CLASSES)):
+        if cls == ignore_index:
+            continue
+
+        # 現在のクラスに対するマスクを作成
+        pred_cls = (pred == cls)
+        target_cls = (target == cls)
+
+        # 各クラスの交差部分と和部分を計算
+        intersection = torch.sum(pred_cls & target_cls).float()
+        union = torch.sum(pred_cls | target_cls).float()
+
+        # IoUを計算、クラスが存在しない場合は0にする
+        if union == 0:
+            iou = torch.tensor(float('nan'))  # NaNとして追加
+        else:
+            iou = intersection / union
+
+        ious.append(iou)
     
+    # NaNを無視して平均IoUを計算
+    ious_tensor = torch.tensor(ious)
+    miou = torch.mean(ious_tensor[~torch.isnan(ious_tensor)])  # NaNを無視して平均
+    return miou
+
 class DiceCrossEntropyLoss(nn.Module):
     def __init__(self, num_classes=22, weight=None, dice_weight=0.5):
         super(DiceCrossEntropyLoss, self).__init__()
